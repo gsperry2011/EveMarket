c320b9270f37fb37fe746e513f2d191feb6fd4a0
gsperry2011 <gsperry2011@gmail.com>
2016-08-23 21:07:25 -0400

    Update LXMLScraper.py

--- LXMLScraper.py
+++ LXMLScraper.py
@@ -1,25 +1,29 @@
-from lxml import html  # Used to parse XML
-import requests
+def LXMLScraper(itemtypeid1, itemtypeid2, regionid):
+    from lxml import html  # Used to parse XML
+    import requests
 
-itemtypeid1 = 34
-itemtypeid2 = 35
-regionid = 10000002
+    itemtypeid1 = 34
+    itemtypeid2 = 35
+    regionid = 10000002
 
-webpage = requests.get('http://api.eve-central.com/api/marketstat?typeid=%i&typeid=%i&regionlimit=%i' % (
-itemtypeid1, itemtypeid2, regionid))
+    webpage = requests.get('http://api.eve-central.com/api/marketstat?typeid=%i&typeid=%i&regionlimit=%i' % (
+    itemtypeid1, itemtypeid2, regionid))
 
-if webpage.status_code == 200:
-    data = html.fromstring(webpage.content)
-    for item in data.iter('type'):
-        #print(item.get("id")) # print node attribute id value.
+    if webpage.status_code == 200:
+        data = html.fromstring(webpage.content)
+        for item in data.iter('type'):
 
-        buy_dict = {node.tag: node.text for node in item.xpath("buy/*")}
-        sell_dict = {node.tag: node.text for node in item.xpath("sell/*")}
+            # print node attribute id value.
+            itemid = (item.get("id"))
+            print(itemid)
 
-        print buy_dict
-        print sell_dict
+            buy_dict = {node.tag: node.text for node in item.xpath("buy/*")}
+            sell_dict = {node.tag: node.text for node in item.xpath("sell/*")}
 
-#Fail if page unavaliable
-else:
-        print "Webpage unavaliable"
-        Webpage.raise_for_status()
+            print buy_dict
+            print sell_dict
+
+    #Fail if page unavaliable
+    else:
+            print "Webpage unavaliable"
+            Webpage.raise_for_status()
